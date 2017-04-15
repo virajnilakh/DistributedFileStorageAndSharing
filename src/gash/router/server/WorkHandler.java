8e53824bc81004629dc8d10ca771f15818c55dfe
pshrutiii <contactdevshruti@gmail.com>
2017-04-14 17:35:07 -0700

    write across nodes

--- src/gash/router/server/WorkHandler.java
+++ src/gash/router/server/WorkHandler.java
@@ -15,9 +15,19 @@
  */
 package gash.router.server;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.protobuf.ByteString;
+
+import gash.router.container.RoutingConf;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
@@ -39,12 +49,20 @@ import routing.Pipe.CommandMessage;
  */
 public class WorkHandler extends SimpleChannelInboundHandler<WorkMessage> {
 	protected static Logger logger = LoggerFactory.getLogger("work");
+	protected ArrayList<CommandMessage> lstMsg = new ArrayList<CommandMessage>();
+	protected ArrayList<ByteString> chunkedFile = new ArrayList<ByteString>();
 	protected ServerState state;
 	protected boolean debug = false;
-
+	protected RoutingConf conf;
 	public WorkHandler(ServerState state) {
 		if (state != null) {
 			this.state = state;
+		}
+	}
+	
+	public WorkHandler(RoutingConf conf) {
+		if (conf != null) {
+			this.conf = conf;
 		}
 	}
 
@@ -123,8 +141,65 @@ public class WorkHandler extends SimpleChannelInboundHandler<WorkMessage> {
 		handleMessage(msg, ctx.channel());
 	}
 
-	private void handleMessage(CommandMessage msg, Channel channel) {
+	private void handleMessage(CommandMessage msg, Channel channel) throws IOException {
 		// TODO Auto-generated method stub
+		
+		if (msg == null) {
+			System.out.println("ERROR: Unexpected content - " + msg);
+			return;
+		}
+		PrintUtil.printCommand(msg);
+		lstMsg.add(msg);
+		
+		if (chunkedFile.size() == msg.getReqMsg().getRwb().getNumOfChunks()) {
+
+			// Sorting
+			Collections.sort(lstMsg, new Comparator<CommandMessage>() {
+				@Override
+				public int compare(CommandMessage msg1, CommandMessage msg2) {
+					return Integer.compare(msg1.getReqMsg().getRwb().getChunk().getChunkId(),
+							msg2.getReqMsg().getRwb().getChunk().getChunkId());
+				}
+			});
+
+			for (CommandMessage message : lstMsg) {
+				chunkedFile.add(message.getReqMsg().getRwb().getChunk().getChunkData());
+			}
+
+			File file = new File("C:\\Gossamer\\" + msg.getReqMsg().getRwb().getFilename());
+			file.createNewFile();
+
+			FileOutputStream outputStream = new FileOutputStream(file);
+			ByteString bs = ByteString.copyFrom(chunkedFile);
+			outputStream.write(bs.toByteArray());
+			outputStream.flush();
+			outputStream.close();
+			System.out.println("File done");
+			long end = System.currentTimeMillis();
+			System.out.println("End time");
+			System.out.println(end);
+		}
+		
+		try {
+			if (msg.hasPing()) {
+				logger.info("ping from " + msg.getHeader().getNodeId());
+			} else if (msg.hasMessage()) {
+				logger.info(msg.getMessage());
+			} else {
+			}
+
+		} catch (Exception e) {
+			// TODO add logging
+			Failure.Builder eb = Failure.newBuilder();
+			eb.setId(conf.getNodeId());
+			eb.setRefId(msg.getHeader().getNodeId());
+			eb.setMessage(e.getMessage());
+			CommandMessage.Builder rb = CommandMessage.newBuilder(msg);
+			rb.setErr(eb);
+			channel.write(rb.build());
+		}
+
+		System.out.flush();
 		
 	}
 
