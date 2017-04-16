6a995a8f796136dece31da9459b467e2d1edfa99
virajnilakh <virajnilakh220@gmail.com>
2017-04-15 17:36:35 -0700

    working iteration 3

--- src/gash/router/server/edges/EdgeMonitor.java
+++ src/gash/router/server/edges/EdgeMonitor.java
@@ -200,8 +200,11 @@ public class EdgeMonitor implements EdgeListener, Runnable {
 				            // Start the client.
 				            //System.out.println("Connect to a node.");
 
-				            final ChannelFuture f = b.connect(host, port);
-				            f.addListener(new FutureListener<Void>() {
+				            final ChannelFuture f = b.connect(host, port).sync();
+				            ei.setChannel(f.channel());
+				            ei.setActive(true);
+				            activeOutboundEdges++;
+				            /*f.addListener(new FutureListener<Void>() {
 
 				                @Override
 				                public void operationComplete(Future<Void> future) throws Exception {
@@ -215,11 +218,12 @@ public class EdgeMonitor implements EdgeListener, Runnable {
 							            activeOutboundEdges++;
 				                    }
 				                }
-				            });				            
+				            });	*/			            
 				            
 				            // Wait until the connection is closed.
 				            //f.channel().closeFuture().sync();
-				        } finally {
+				        } catch(Exception e) {
+				        	e.printStackTrace();
 				            //workerGroup.shutdownGracefully();
 				        }
 
