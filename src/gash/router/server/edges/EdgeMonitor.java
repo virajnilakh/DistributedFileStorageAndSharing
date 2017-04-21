387706e1d04f2535a944d36bf28b9f9f5bcee672
virajnilakh <virajnilakh220@gmail.com>
2017-04-21 01:11:17 -0700

    minor changes

--- src/gash/router/server/edges/EdgeMonitor.java
+++ src/gash/router/server/edges/EdgeMonitor.java
@@ -152,6 +152,8 @@ public class EdgeMonitor implements EdgeListener, Runnable {
 		hb.setTime(System.currentTimeMillis());
 		LeaderStatus.Builder status=LeaderStatus.newBuilder();
 		status.setLeaderId(state.getConf().getNodeId());
+		status.setLeaderHost(state.getIpAddress());
+
 		WorkMessage.Builder wb = WorkMessage.newBuilder();
 		wb.setHeader(hb);
 		wb.setSecret(0);
